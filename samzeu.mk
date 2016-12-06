6486f8a9b431f7919e9895483e33571f70007ab3
lo52tp <lo52tp@lo52tp-VirtualBox.(none)>
2016-12-06 11:10:26 +0100

    essai de production de l'image android, Erreur courante : build/core/base_rules.mk:166: *** external/libpng: MODULE.TARGET.SHARED_LIBRARIES.libusb already defined by device/ti/uim. Arrêt.

--- samzeu.mk
+++ samzeu.mk
@@ -4,7 +4,7 @@ DEVICE_PACKAGE_OVERLAYS := device/utbm/samzeu/overlay
 
 PRODUCT_COPY_FILES += device/utbm/samzeu/system/media/bootanimation.zip:system/media/bootanimation.zip
 
-PRODUCT_PACKAGES += external/libusb
+PRODUCT_PACKAGES += libusb
 
 PRODUCT_NAME:= samzeu
 PRODUCT_DEVICE:= samzeu
